6c66e97dede5071b71a65a3a273c378974dceb6d
Sakthivel <sakthi.samuthiram@gmail.com>
2023-10-06 17:02:18 +0530

    Refactored the hooks and introduced lookup hooks

--- src/components/PlatformFilter.tsx
+++ src/components/PlatformFilter.tsx
@@ -1,8 +1,7 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import usePlatforms from "../hooks/usePlatforms";
 import { BsChevronDown } from "react-icons/bs";
-import { Platform } from "../hooks/usePlatforms";
-import { useState } from "react";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   onSelectedPlatform: (platformId: number) => void;
@@ -10,9 +9,7 @@ interface Props {
 }
 const PlatformFilter = ({ onSelectedPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms();
-  const selectedPlatform = data?.results.find(
-    (p) => p.id === selectedPlatformId
-  );
+  const selectedPlatform = usePlatform(selectedPlatformId);
 
   if (error) return null;
   return (
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -10,14 +10,12 @@ export interface Genre {
   image_background: string;
 }
 const client = new APIClient("/genres");
-//const useGenres = () => useData<Genre>("/genres");
-//const useGenres = () => ({ data: genre, isLoading: false, error: null });
 
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: genre.length, results: genre },
+    initialData: genre,
   });
 export default useGenres;
